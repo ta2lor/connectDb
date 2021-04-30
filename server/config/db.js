a852e40589e2e198c34f806a3aace1ef59e56cba
ta2lor <63273173+ta2lor@users.noreply.github.com>
2021-04-30 10:53:22 +0900

    Delete db.js

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require("mysql");
-const db = mysql.createPool({
-  host: "http://cloud.park-cloud.co19.kr/",
-  user: "root",
-  password: "134625",
-  database: "test2",
-});
-
-module.exports = db;
