1c501a2eec36169c1cbcaf28ca3de6bb789a1159
ta2lor <ksh262011@naver.com>
2021-04-30 10:54:24 +0900

    connect db success

--- server/server.js
+++ server/server.js
@@ -9,12 +9,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/db", (req, res) => {
-  db.query("select * from temp1", (err, data) => {
+  db.query("select temp from test", (err, data) => {
     if (!err) res.send({ products: data });
     else res.send(err);
   });
+  db.release();
 });
 
+app.get("/hello", (req, res) => {
+  res.send({ hello: "HELLO REACT from node server.js" });
+});
 app.listen(PORT, () => {
   console.log(`Server on http://localhost:${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <h1>hello</h1>;
-}
+import React, { Component } from "react";
+import axios from "axios";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: [],
+    };
+  }
+  componentDidMount() {
+    this._getHello();
+  }
+  _getHello = async () => {
+    const res = await axios.get("/api/db");
+    this.setState({ hello: res.data.products });
+    console.log(this.state.hello);
+  };
+
+  render() {
+    return (
+      <>
+        <h3>
+          {" "}
+          {this.state.hello.map((hello1) => (
+            <div>{hello1.temp}</div>
+          ))}{" "}
+          - connected to server from App.js{" "}
+        </h3>
+      </>
+    );
+  }
+}
 export default App;
